2b3530b46522d92fb06b6e30b97e7564cb5ed28b
Alexandra Klimenko <klimenko@usc.edu>
2017-05-15 16:13:41 -0700

    Arka Challenge

---
+++ MyArkaProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyArkaProject.Startup))]
+namespace MyArkaProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
